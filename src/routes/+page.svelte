90a15910b9d45adf06fc98ee43a66da4c4dbad84
Silvestrs <SilvestrsL47@gmail.com>
2023-10-11 15:04:39 +0300

    Jonatana Biedena pēdejais? upuris.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,27 @@
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Pixelify+Sans&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Chakra+Petch&display=swap');
 * {
-    font-family: 'Pixelify Sans', sans-serif;
+    font-family: 'Chakra Petch', sans-serif;
+}
+p {
+    font-size: 24px;
 }
 </style>
 
 <h1>Kas ir prasības?</h1>
-<p></p>
+<p>
+Prasības programmēšanā ir procesa daļa, kurā tiek definētas un dokumentētas sistēmas vai programmatūras prasības. Tas ietver prasību analīzi, definēšanu un specifikāciju.
+
+Prasības programmēšana ir svarīgas, jo tā nodrošina skaidru pamatu produktu izmaksu, risku un grafiku aplēsēm. Tā arī samazina vajadzību pēc vēlākas pārprojektēšanas.
+    
+Šis process var ietvert vairākus posmus:
+    
+Analīze: Pirms sistēmas izstrādnes ir nepieciešams definēt tai veicamos uzdevumus.
+Projektēšana: Visas prasības jaunajai sistēmai tiek sadalītas divās lielās grupās – tās, kas attiecas uz aparatūru (datori, tīkli, ievadierīces, izvadierīces u. c.), un tās, kuras attiecas uz programmatūru.
+Programmēšana: Datorprogrammu izstrāde ietver problēmas risināšanas algoritma detalizēšanu un tā pierakstīšanu attiecīgajā programmēšanas valodā, kā arī datu struktūras izvēli un to kodēšanu.
+Testēšana: Programmētāji veic sistēmas testēšanu, lai pārliecinātos, ka tā darbojas pareizi un visas nepieciešamās programmatūras vajadzības ir realizētas.
+Prasību programmēšana ir būtiska programmatūras izstrādes sastāvdaļa, kas palīdz nodrošināt, ka gala produkts atbilst klienta vajadzībām un gaidītajai funkcionalitātei.
+</p>
 <h1>Kas ir prasību specifikācija?</h1>
 <p>
 Prasību specifikācija ir dokumentācijas veids, kas apraksta, ko sistēma vai programmatūra ir jādara. Tā definē konkrētas prasības, kas jāizpilda sistēmai vai programmatūrai, un var ietvert funkcionalitātes, interfeisu, darbības un citus aspektus.
@@ -16,4 +31,12 @@ Prasību specifikācija ir svarīga, jo tā nodrošina skaidru pamatu produktu i
 Šis dokuments kalpo kā līgums starp pasūtītāju un izstrādātāju, nosakot precīzas prasības, kas jāizpilda. Tas var būt ļoti svarīgs, lai nodrošinātu, ka gala produkts atbilst klienta vajadzībām un gaidītajai funkcionalitātei.
 </p>
 <h1>Un ar ko viena atšķirās no otras?</h1>
-<p></p>
+<p>
+Prasības un specifikācijas ir divi atšķirīgi, bet savstarpēji saistīti jēdzieni.
+
+Prasības ir definīcijas par to, ko sistēma vai produkts ir jādara. Tās var aprakstīt funkcionalitāti, darbību, interfeisu un citus aspektus. Prasības ir svarīgas, lai nodrošinātu skaidru pamatu produktu izmaksu, risku un grafiku aplēsēm1. Tās arī samazina vajadzību pēc vēlākas pārprojektēšanas.
+    
+Specifikācijas, no otras puses, apraksta konkrētas prasības, kas jāizpilda sistēmai vai produktam. Tās var ietvert prasības par materiāliem, izpildes metodi, kvalitāti un citiem aspektiem. Specifikācijas nodrošina skaidru pamatu darba izpildes un kvalitātes novērtējumam.
+    
+Tātad, galvenā atšķirība starp prasībām un specifikācijām ir tāda, ka prasības apraksta ko sistēma vai produkts ir jādara, kamēr specifikācijas apraksta kā šīs prasības ir jāizpilda.
+</p>
